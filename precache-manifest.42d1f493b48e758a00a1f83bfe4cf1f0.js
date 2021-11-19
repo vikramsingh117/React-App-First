e05954590d8071ee86db1caa9000b79ac91abb23
vikram <vikramandanshu@gmail.com>
2021-11-19 16:01:51 +0530

    Updates

---
+++ precache-manifest.42d1f493b48e758a00a1f83bfe4cf1f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990c4ba9b181f9b30aa336de9b08efeb",
+    "url": "/React-App-First/index.html"
+  },
+  {
+    "revision": "2b2f730fc3d57818e095",
+    "url": "/React-App-First/static/css/main.c9f3fafa.chunk.css"
+  },
+  {
+    "revision": "c610cb98b3b0afa11acb",
+    "url": "/React-App-First/static/js/2.e19bcbf5.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/React-App-First/static/js/2.e19bcbf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f17882e3e9299f21a016",
+    "url": "/React-App-First/static/js/3.7c72e190.chunk.js"
+  },
+  {
+    "revision": "2b2f730fc3d57818e095",
+    "url": "/React-App-First/static/js/main.94ae8e24.chunk.js"
+  },
+  {
+    "revision": "5d1b0ea127cf8748af29",
+    "url": "/React-App-First/static/js/runtime-main.f704ce3a.js"
+  }
+]);
